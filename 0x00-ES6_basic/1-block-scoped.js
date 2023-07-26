6a6dc1c2b7dcdedbf26861e384a35d5c46c44bfb
silas-inegbe <kingssilas18@gmail.com>
2023-07-26 15:14:10 +0000

    testin

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,7 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    (function () {
-      var task = true;
-      var task2 = false;
-    })();
+    return [task, task2];
   }
 
   return [task, task2];
