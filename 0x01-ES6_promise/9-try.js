c4b351a91b0a62817f28ff498c207d8a63105a78
silas-inegbe <kingssilas18@gmail.com>
2023-07-31 22:01:35 +0000

    updted

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-	const queue = [];
+  const queue = [];
 
-	try {
-		queue.push(mathFunction());
-	}	catch (err) {
-		queue.push(err.toString());
-	}	finally {
-		queue.push('Guardrail was processed');
-	}
-	return queue;
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
