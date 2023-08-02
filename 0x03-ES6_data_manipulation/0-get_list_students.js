694b59f1fcfca4066d09f0803a4d59cf30d7b450
silas-inegbe <kingssilas18@gmail.com>
2023-08-02 11:13:22 +0000

    test

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,5 @@
+const getListStudents = [
+	{ id: 1, firstname: 'Guillaume', location: 'San Francisco'},
+	{ id: 2, firstname: 'James', location: 'Columbia' },
+	{ id: 3, firstname: 'Serena', location: 'San Francisco' }
+];
