dc7e35b5bdd35de824792e90a355c99df7dbcd6f
silas-inegbe <kingssilas18@gmail.com>
2023-08-04 23:03:32 +0000

    created some classes and interface with typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,49 @@
+interface Teacher {
+	readonly firtstName: string;
+	readonly lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience: number;
+	location: string;
+	contract: boolean;
+	[propName: string]: any;
+}
+
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+interface printTeacherFunction {
+	firstName: string;
+	LastName: string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastname) => {
+	return `${firstName[0]}. ${lastName}`;
+};
+
+interface StudentConstructor {
+	new (firstName; string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+};
+
+export { printTeacher, StudentClass };
